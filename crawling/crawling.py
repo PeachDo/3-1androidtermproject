d26c2424566b46961a378a2c20976f01a49b73d8
jiwon kim <rlawldnjs8@gmail.com>
2020-05-15 17:58:05 +0900

    Update crawling.py

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -9,7 +9,7 @@ context = ssl._create_unverified_context()
 
 baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
 plusUrl = input('Please enter your keyword to search : ')
-# 한글 검색 자동 변환
+
 url = baseUrl + quote_plus(plusUrl)
 html = urlopen(url, context=context)
 soup = bs(html, "html.parser")
@@ -19,7 +19,7 @@ n = 1
 for i in img:
     imgUrl = i['data-source']
     with urlopen(imgUrl, context=context) as f:
-        with open('/Users/jiwon/PycharmProjects/crawling/female/cat/' + plusUrl + str(n)+'.jpg', 'wb') as h: # w - write b - binary
+        with open('/Users/jiwon/PycharmProjects/crawling/female/cat/' + plusUrl + str(n)+'.jpg', 'wb') as h: 
             img = f.read()
             h.write(img)
     n += 1
